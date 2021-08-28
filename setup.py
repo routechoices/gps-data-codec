b00b00121cd48f74e2da6a510513dc4448310a31
Raphaël Stefanini <raphael@rphlo.com>
2021-08-28 08:39:19 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="gps-encoding-rust",
-    version="1.1.0",
+    version="1.1.1",
     rust_extensions=[RustExtension("gps_encoding.gps_encoding", binding=Binding.RustCPython)],
     packages=["gps_encoding"],
     # rust extensions are not zip safe, just like C-extensions.
