11625c8af86fe8771708b6ce3f12becc5f6527ea
Raphaël Stefanini <raphael@rphlo.com>
2022-01-27 14:00:34 +0200

    Add decoding functionality

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="gps-encoding-rust",
-    version="1.1.1",
+    version="1.2.0",
     rust_extensions=[RustExtension("gps_encoding.gps_encoding", binding=Binding.RustCPython)],
     packages=["gps_encoding"],
     # rust extensions are not zip safe, just like C-extensions.
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,86 @@
 extern crate cpython;
 
-use cpython::{Python, PyErr, PyResult, PyDict , PyList, py_module_initializer, py_fn, exc};
+use cpython::{Python, PyErr, PyResult, PyDict, PythonObject, PyList, py_module_initializer, py_fn, exc};
 use std::char;
 
 
 py_module_initializer!(gps_encoding, |py, m| {
-    m.add(py, "__doc__", "This module is implemented in Rust.")?;
-    m.add(py, "encode_unsigned_number", py_fn!(py, encode_unsigned_number(num: i64)))?;
-    m.add(py, "encode_signed_number", py_fn!(py, encode_signed_number(num: i64)))?;
+    m.add(py, "__doc__", "Encode/Decode GPS data")?;
     m.add(py, "encode_data", py_fn!(py, encode_data(data: &PyList)))?;
+    m.add(py, "decode_data", py_fn!(py, decode_data(encoded: String)))?;
     Ok(())
 });
 
+struct DecodingResult {
+    result: i64,
+    consumed: u32,
+}
 
-fn encode_unsigned_number_rust(num: i64) -> String  {
+fn decode_unsigned_value_from_string(encoded: &String, offset: u32) -> DecodingResult {
+    let enc_len: u32 = encoded.len() as u32;
+    let mut i: u32 =0;
+    let mut s: u32 = 0;
+    let mut result: i64 = 0;
+    let mut b: u8 = 0x20;
+    let ptr: *const u8 = encoded.as_ptr();
+    while b >= 0x20 && i + offset < enc_len {
+        assert!(i + offset < enc_len);
+        unsafe {
+            b = *ptr.offset((i + offset) as isize) as u8 - 63;
+        }
+        i += 1;
+        result |=  ((b & 0x1f) as i64) << s;
+        s += 5;
+    }
+    return DecodingResult{result: result, consumed: i};
+}
+
+fn decode_signed_value_from_string(encoded: &String, offset: u32) -> DecodingResult {
+    let r: DecodingResult = decode_unsigned_value_from_string(&encoded, offset);
+    let result: i64 = r.result;
+    if result & 1 == 1 {
+        return DecodingResult{result: !(result >> 1), consumed: r.consumed}
+    } else {
+        return DecodingResult{result: result >> 1, consumed: r.consumed}
+    }
+}
+
+pub fn decode_data(_py: Python, input: String) -> PyResult<PyList> {
+    const YEAR2010: i64 = 1262304000;
+    let encoded: &String = &input;
+    let mut vals: Vec<i64> = vec![YEAR2010, 0, 0];
+    let enc_len: u32 = encoded.len() as u32;
+    let mut c: u32 = 0;
+    let mut r: DecodingResult;
+    let mut is_first: bool = true;
+    let res: PyList = PyList::new(_py, &[]);
+
+    while c < enc_len {
+        for i in 0..3 {
+            if i == 0 {
+                if is_first {
+                    is_first = false;
+                    r = decode_signed_value_from_string(encoded, c);
+                } else {
+                    r = decode_unsigned_value_from_string(encoded, c);
+                }
+            } else {
+                r = decode_signed_value_from_string(encoded, c);
+            }
+            c += r.consumed;
+            let new_val: i64 = vals[i] + r.result;
+            vals[i] = new_val;
+        }
+        let pt: PyDict = PyDict::new(_py);
+        pt.set_item(_py, "timestamp", vals[0] as f64);
+        pt.set_item(_py, "latitude", (vals[1] as f64) / 1e5);
+        pt.set_item(_py, "longitude", (vals[2] as f64) / 1e5);
+        res.append(_py, pt.into_object());
+    }
+    Ok(res)
+}
+
+fn encode_unsigned_number(num: i64) -> String  {
     let mut encoded = String::from("");
     let mut tmp: i64 = num;
     while tmp >= 0x20 {
@@ -24,22 +91,12 @@ fn encode_unsigned_number_rust(num: i64) -> String  {
     return encoded;
 }
 
-fn encode_signed_number_rust(num: i64) -> String {
+fn encode_signed_number(num: i64) -> String {
     let mut sgn_num: i64 = num << 1;
     if num < 0 {
         sgn_num = !sgn_num;
     }
-    return encode_unsigned_number_rust(sgn_num);
-}
-
-fn encode_unsigned_number(_py: Python, num: i64) -> PyResult<String>  {
-    let encoded: String = encode_unsigned_number_rust(num);
-    Ok(encoded)
-}
-
-fn encode_signed_number(_py: Python, num: i64) -> PyResult<String>  {
-    let encoded: String = encode_signed_number_rust(num);
-    Ok(encoded)
+    return encode_unsigned_number(sgn_num);
 }
 
 fn encode_data(_py: Python, data: &PyList) -> PyResult<String> {
@@ -61,21 +118,21 @@ fn encode_data(_py: Python, data: &PyList) -> PyResult<String> {
         let tim: i64 = pt.get_item(_py, "timestamp").unwrap().extract(_py)?;
         let tim_d: i64 = tim - prev_t;
         if is_first {
-            result.push_str(&encode_signed_number_rust(tim_d));
+            result.push_str(&encode_signed_number(tim_d));
             is_first = false;
         } else if tim_d < 0 {
             return Err(PyErr::new::<exc::ValueError, _>(_py, "invalid timestamp, list should be sorted by increasing timestamp"));
         } else {
-            result.push_str(&encode_unsigned_number_rust(tim_d));
+            result.push_str(&encode_unsigned_number(tim_d));
         }
 
         let lat: f64 = pt.get_item(_py, "latitude").unwrap().extract(_py)?;
         let lat_d: i64 = ((lat - prev_lat) * 1e5).round() as i64;
-        result.push_str(&encode_signed_number_rust(lat_d));
+        result.push_str(&encode_signed_number(lat_d));
 
         let lon: f64 = pt.get_item(_py, "longitude").unwrap().extract(_py)?;
         let lon_d: i64 = ((lon - prev_lon) * 1e5).round() as i64;
-        result.push_str(&encode_signed_number_rust(lon_d));
+        result.push_str(&encode_signed_number(lon_d));
 
         prev_t += tim_d;
         prev_lat += (lat_d as f64) / 1e5;
