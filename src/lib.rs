b92aa7188987459c126dd8b613dcd1bbc1e48fbc
Raphaël Stefanini <raphael@rphlo.com>
2022-02-12 16:46:54 +0200

    use array for better efficiency

--- src/lib.rs
+++ src/lib.rs
@@ -111,7 +111,7 @@ fn encode_data(_py: Python, data: &PyList) -> PyResult<String> {
     
     for py_pt in data.iter(_py) {
         let pt: PyTuple = py_pt.extract(_py)?;
-        if !pt.len(_py) != 3 {
+        if pt.len(_py) != 3 {
             return Err(PyErr::new::<exc::ValueError, _>(_py, "invalid list, item does not contains a valid GPS data array"));
         }
 
