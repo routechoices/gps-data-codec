81e7212b4cc7b93eb3240b69572f48921d55b633
Raphael Stefanini <raphael@rphlo.com>
2022-09-08 21:24:20 +0300

    Improve encoding performance

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 extern crate cpython;
 
 use cpython::{Python, PyErr, PyResult, PythonObject, PyList, PyTuple, PyFloat, py_module_initializer, py_fn, exc};
-use std::char;
 
 
 py_module_initializer!(gps_encoding, |py, m| {
@@ -77,18 +76,18 @@ pub fn decode_data(_py: Python, input: String) -> PyResult<PyList> {
     Ok(res)
 }
 
-fn encode_unsigned_number(num: i64) -> String  {
-    let mut encoded = String::from("");
+fn encode_unsigned_number(num: i64) -> Vec<u8>  {
+    let mut encoded: Vec<u8> = vec![];
     let mut tmp: i64 = num;
     while tmp >= 0x20 {
-        encoded.push_str(&(((0x20 | (tmp as u8 & 0x1f)) + 63) as char).to_string());
+        encoded.push(((0x20 | (tmp as u8 & 0x1f)) + 63) as u8);
         tmp >>= 5;
     }
-    encoded.push_str(&((tmp as u8 + 63) as char).to_string());
+    encoded.push((tmp as u8 + 63) as u8);
     return encoded;
 }
 
-fn encode_signed_number(num: i64) -> String {
+fn encode_signed_number(num: i64) -> Vec<u8> {
     let mut sgn_num: i64 = num << 1;
     if num < 0 {
         sgn_num = !sgn_num;
@@ -102,7 +101,7 @@ fn encode_data(_py: Python, data: &PyList) -> PyResult<String> {
     let mut prev_lat: f64 = 0.0;
     let mut prev_lon: f64 = 0.0;
 
-    let mut result: String = "".to_owned();
+    let mut result: Vec<u8> = vec![];
     
     let mut is_first: bool = true;
     
@@ -115,25 +114,25 @@ fn encode_data(_py: Python, data: &PyList) -> PyResult<String> {
         let tim: f64 = pt.get_item(_py, 0).extract(_py)?;
         let tim_d: i64 = tim.round() as i64 - prev_t;
         if is_first {
-            result.push_str(&encode_signed_number(tim_d));
+            result.append(&mut encode_signed_number(tim_d));
             is_first = false;
         } else if tim_d < 0 {
             return Err(PyErr::new::<exc::ValueError, _>(_py, "invalid timestamp, list should be sorted by increasing timestamp"));
         } else {
-            result.push_str(&encode_unsigned_number(tim_d));
+            result.append(&mut encode_unsigned_number(tim_d));
         }
 
         let lat: f64 = pt.get_item(_py, 1).extract(_py)?;
         let lat_d: i64 = ((lat - prev_lat) * 1e5).round() as i64;
-        result.push_str(&encode_signed_number(lat_d));
+        result.append(&mut encode_signed_number(lat_d));
 
         let lon: f64 = pt.get_item(_py, 2).extract(_py)?;
         let lon_d: i64 = ((lon - prev_lon) * 1e5).round() as i64;
-        result.push_str(&encode_signed_number(lon_d));
+        result.append(&mut encode_signed_number(lon_d));
 
         prev_t += tim_d;
         prev_lat += (lat_d as f64) / 1e5;
         prev_lon += (lon_d as f64) / 1e5;
     }
-    Ok(result)
+    Ok(String::from_utf8(result).unwrap())
 }
