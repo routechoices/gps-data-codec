e93060210ff40a8ba03a36f6c808d4f75c6fe222
Raphaël Stefanini <rphlo@users.noreply.github.com>
2023-06-02 13:00:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import gps_data_codec
 def test_lib():
     input = [(-1,0,0),(1628667993, 4.56543, -110.53621), (1628667994, 4.56553, -110.53625)]
     expected_encoded = '`o|sfjA??ya_fpo@}tzZhbtaT@SF'
-    encoded = gps_data_codec.encode_data(input)
+    encoded = gps_data_codec.encode(input)
     assert(encoded == expected_encoded)
-    output = gps_data_codec.decode_data(encoded)
+    output = gps_data_codec.decode(encoded)
     assert(output == input)
 
 
