e27896a1f7ba73bc12b538392f44d6b6e517a102
Raphael Stefanini <raphael@rphlo.com>
2023-10-04 17:54:37 +0300

    Clean up code

--- src/lib.rs
+++ src/lib.rs
@@ -2,74 +2,40 @@ use pyo3::prelude::*;
 use pyo3::exceptions::PyValueError;
 use pyo3::types::{PyTuple, PyList};
 
-#[pymodule]
-fn gps_data_codec(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(encode, m)?)?;
-    m.add_function(wrap_pyfunction!(decode, m)?)?;
-    Ok(())
-}
-
 struct DecodingResult {
-    result: i64,
+    value: i64,
     consumed: u32,
 }
 
-fn decode_unsigned_value_from_string(encoded: &[u8], offset: u32) -> DecodingResult {
-    let enc_len: u32 = encoded.len() as u32;
-    let mut i: u32 = 0;
-    let mut s: u32 = 0;
-    let mut result: i64 = 0;
-    let mut b: u8 = 0x20;
-    while b >= 0x20 && i + offset < enc_len {
-        assert!(i + offset < enc_len);
-        b = encoded[(i + offset) as usize] - 63;
-        i += 1;
-        result |=  ((b & 0x1f) as i64) << s;
-        s += 5;
+fn decode_unsigned_value_from_string(encoded: &[u8], offset: u32, encoded_length: u32) -> DecodingResult {
+    let mut value: i64 = 0;
+    let mut consumed: u32 = 0;
+    let mut size: u32 = 0;
+    let mut byte: u8 = 0x20;
+    while byte >= 0x20 && consumed + offset < encoded_length {
+        assert!(consumed + offset < encoded_length);
+        byte = encoded[(consumed + offset) as usize] - 63;
+        consumed += 1;
+        value |=  ((byte & 0x1f) as i64) << size;
+        size += 5;
     }
-    return DecodingResult{result: result, consumed: i};
+    return DecodingResult{value: value, consumed: consumed};
 }
 
-fn decode_signed_value_from_string(encoded: &[u8], offset: u32) -> DecodingResult {
-    let r: DecodingResult = decode_unsigned_value_from_string(encoded, offset);
-    let result: i64 = r.result;
-    if result & 1 == 1 {
-        return DecodingResult{result: !(result >> 1), consumed: r.consumed}
-    } else {
-        return DecodingResult{result: result >> 1, consumed: r.consumed}
-    }
-}
-
-#[pyfunction]
-fn decode(_py: Python, input: String) -> PyResult<PyObject> {    
-    const YEAR2010: i64 = 1262304000;
-    let encoded: &[u8] = &input.as_bytes();
-    let mut vals: Vec<i64> = vec![YEAR2010, 0, 0];
-    let enc_len: u32 = encoded.len() as u32;
-    let mut c: u32 = 0;
-    let mut r: DecodingResult;
-    let mut is_first: bool = true;
-    let res: &PyList = PyList::empty(_py);
-    while c < enc_len {
-        for i in 0..3 {
-            if i == 0 {
-                if is_first {
-                    is_first = false;
-                    r = decode_signed_value_from_string(encoded, c);
-                } else {
-                    r = decode_unsigned_value_from_string(encoded, c);
-                }
-            } else {
-                r = decode_signed_value_from_string(encoded, c);
-            }
-            c += r.consumed;
-            let new_val: i64 = vals[i] + r.result;
-            vals[i] = new_val;
+fn decode_signed_value_from_string(encoded: &[u8], offset: u32, encoded_length: u32) -> DecodingResult {
+    let tmp_result: DecodingResult = decode_unsigned_value_from_string(encoded, offset, encoded_length);
+    let tmp_value: i64 = tmp_result.value;
+    if tmp_value & 1 == 1 {
+        return DecodingResult{
+            value: !(tmp_value >> 1),
+            consumed: tmp_result.consumed
         }
-        let pt: &PyTuple = PyTuple::new(_py, [(vals[0] as i64).to_object(_py), ((vals[1] as f64) / 1e5).to_object(_py), ((vals[2] as f64) / 1e5).to_object(_py)]);
-        res.append(pt)?;
+    } else {
+        return DecodingResult{
+            value: tmp_value >> 1,
+            consumed: tmp_result.consumed
+        }
     }
-    Ok(res.to_object(_py))
 }
 
 fn encode_unsigned_number(num: i64) -> Vec<u8>  {
@@ -92,44 +58,85 @@ fn encode_signed_number(num: i64) -> Vec<u8> {
 }
 
 #[pyfunction]
+fn decode(_py: Python, input: String) -> PyResult<PyObject> {    
+    const YEAR2010: i64 = 1262304000;
+    let mut values: [i64; 3] = [YEAR2010, 0, 0];
+    let mut bytes_consumed: u32 = 0;
+    let mut decoding_result: DecodingResult;
+    let encoded: &[u8] = &input.as_bytes();
+    let encoded_length: u32 = encoded.len() as u32;
+    let output: &PyList = PyList::empty(_py);
+    while bytes_consumed < encoded_length {
+        for i in 0..3 {
+            if i == 0 {
+                if bytes_consumed == 0 {
+                    decoding_result = decode_signed_value_from_string(encoded, bytes_consumed, encoded_length);
+                } else {
+                    decoding_result = decode_unsigned_value_from_string(encoded, bytes_consumed, encoded_length);
+                }
+            } else {
+                decoding_result = decode_signed_value_from_string(encoded, bytes_consumed, encoded_length);
+            }
+            values[i] += decoding_result.value;
+            bytes_consumed += decoding_result.consumed;
+        }
+        output.append(
+            PyTuple::new(
+                _py,
+                [
+                    (values[0] as i64).to_object(_py),
+                    ((values[1] as f64) / 1e5).to_object(_py),
+                    ((values[2] as f64) / 1e5).to_object(_py)
+                ]
+            )
+        )?;
+    }
+    Ok(output.to_object(_py))
+}
+
+#[pyfunction]
 fn encode(_py: Python, data: &PyList) -> PyResult<String> {
     const YEAR2010: i64 = 1262304000;
-    let mut prev_t: i64 = YEAR2010;
-    let mut prev_lat: f64 = 0.0;
-    let mut prev_lon: f64 = 0.0;
+    let mut prev_timestamp: i64 = YEAR2010;
+    let mut prev_latitude: f64 = 0.0;
+    let mut prev_longitude: f64 = 0.0;
 
-    let mut result: Vec<u8> = vec![];
-    
-    let mut is_first: bool = true;
-    
-    for py_pt in data.iter() {
-        let pt: &PyTuple = py_pt.downcast::<PyTuple>()?;
-        if pt.len() != 3 {
+    let mut output: Vec<u8> = vec![];
+
+    for (point_count, point_object) in data.iter().enumerate() {
+        let point_data: &PyTuple = point_object.downcast::<PyTuple>()?;
+        if point_data.len() != 3 {
             return Err(PyValueError::new_err("invalid list, item does not contains a valid GPS data array"));
         }
 
-        let tim: f64 = pt[0].extract::<f64>()?;
-        let tim_d: i64 = tim.round() as i64 - prev_t;
-        if is_first {
-            result.append(&mut encode_signed_number(tim_d));
-            is_first = false;
-        } else if tim_d < 0 {
-            return Err(PyValueError::new_err("invalid timestamp, list should be sorted by increasing timestamp"));
+        let timestamp: f64 = point_data[0].extract::<f64>()?;
+        let timestamp_diff: i64 = timestamp.round() as i64 - prev_timestamp;
+        if point_count == 0 {
+            output.append(&mut encode_signed_number(timestamp_diff));
+        } else if timestamp_diff >= 0 {
+            output.append(&mut encode_unsigned_number(timestamp_diff));
         } else {
-            result.append(&mut encode_unsigned_number(tim_d));
+            return Err(PyValueError::new_err("invalid timestamp, list should be sorted by increasing timestamp"));
         }
 
-        let lat: f64 = pt[1].extract::<f64>()?;
-        let lat_d: i64 = ((lat - prev_lat) * 1e5).round() as i64;
-        result.append(&mut encode_signed_number(lat_d));
+        let latitude: f64 = point_data[1].extract::<f64>()?;
+        let latitude_diff: i64 = ((latitude - prev_latitude) * 1e5).round() as i64;
+        output.append(&mut encode_signed_number(latitude_diff));
 
-        let lon: f64 = pt[2].extract::<f64>()?;
-        let lon_d: i64 = ((lon - prev_lon) * 1e5).round() as i64;
-        result.append(&mut encode_signed_number(lon_d));
+        let longitude: f64 = point_data[2].extract::<f64>()?;
+        let longitude_diff: i64 = ((longitude - prev_longitude) * 1e5).round() as i64;
+        output.append(&mut encode_signed_number(longitude_diff));
 
-        prev_t += tim_d;
-        prev_lat += (lat_d as f64) / 1e5;
-        prev_lon += (lon_d as f64) / 1e5;
+        prev_timestamp += timestamp_diff;
+        prev_latitude += (latitude_diff as f64) / 1e5;
+        prev_longitude += (longitude_diff as f64) / 1e5;
     }
-    Ok(String::from_utf8(result).unwrap())
+    Ok(String::from_utf8(output).unwrap())
+}
+
+#[pymodule]
+fn gps_data_codec(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(encode, m)?)?;
+    m.add_function(wrap_pyfunction!(decode, m)?)?;
+    Ok(())
 }
